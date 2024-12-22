6ce92cfc830a1557dbbd9a098d3c24ec40f856fa
Grégory Chariot <chariot.gregory@gmail.com>
2024-12-22 12:51:09 +0100

    up

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC12V7hUaPCuJQl6vCD_6tbQfVkUI6yVb4",
-  authDomain: "liste-course-e65d3.firebaseapp.com",
-  databaseURL:
-    "https://liste-course-e65d3-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "liste-course-e65d3",
-  storageBucket: "liste-course-e65d3.firebasestorage.app",
-  messagingSenderId: "1069804962848",
-  appId: "1:1069804962848:web:f62b9adf37787806e36845",
-  measurementId: "G-8GYQ80XBWB",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
